3fe4aef8caf5489bfacd50c44a919f4d406c136b
Ashwini-Pandya <ashwinipandya5@gmail.com>
2025-06-26 19:24:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [quotes, setQuotes] = useState({ content: "", author: "" });
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch("https://api.quotable.io/random/");
+    const data = await response.json();
+    setQuotes({
+      content: data.content,
+      author: data.author,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title"> Random Quote-Generator </h1>
+      <div className="quote-box">
+        <div className="container">
+          <h2 className="quote-txt"> {quotes.content} </h2>
+          <h2 className="quote-author"> ~ {quotes.author}</h2>
+        </div>
+
+        <button onClick={fetchData} disabled={loading}>
+          {loading ? "Loading..." : "Click to Change"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
